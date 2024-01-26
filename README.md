a1a3e9351fdd055a8203a82be084890c069ed36b
kabuzo13 <beahonigmann@gmail.com>
2024-01-26 14:28:21 +0000

    completed README file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # work-scheduler
+
+## Description
+
+A work scheduler with a live time and date that allows you to add your own appointments in the workday timeslots. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Type to add in an appointment and then click the save button for the timeslot to save it to your schedule. Past timeslots are greyed out, the current timeslot is red, and future timeslots are green. 
+
+## Credits
+
+Used starter code from edX Boot Camps LLC and help from the 1:1 tutoring service. 
+
+## License
+
+Please refer to the MIT License in the repo. 
--- starter/script.js
+++ starter/script.js
@@ -1,65 +1,18 @@
 $(document).ready(function(){
 
 var myInterval = setInterval(function(){
-    var currentDate = dayjs().format('dddd MMMM Do YYYY');
+    var currentDate = dayjs().format('dddd MMMM D YYYY');
     var currentTime = dayjs()
     $('#currentDay').text(currentDate + " " + currentTime.format('h:mm:ss a'))
 }, 1000);
 
-// var timeBlockEl9 = $('div');
-// var timeBlockEl10 = $('div');
-// var timeBlockEl11 = $('div');
-// var timeBlockEl12 = $('div');
-// var timeBlockEl13 = $('div');
-// var timeBlockEl14 = $('div');
-// var timeBlockEl15 = $('div');
-// var timeBlockEl16 = $('div');
-// var timeBlockEl17 = $('div');
-
-// $('#container').append(timeBlockEl9);
-// $('#container').append(timeBlockEl10);
-// $('#container').append(timeBlockEl11);
-// $('#container').append(timeBlockEl12);
-// $('#container').append(timeBlockEl13);
-// $('#container').append(timeBlockEl14);
-// $('#container').append(timeBlockEl15);
-// $('#container').append(timeBlockEl16);
-// $('#container').append(timeBlockEl17);
-
-// timeBlockEl9.addClass('time-block row hour past present future');
-// timeBlockEl9.text('9am');
-
-// timeBlockEl10.addClass('time-block row hour past present future');
-// timeBlockEl10.text('10am');
-
-// timeBlockEl11.addClass('time-block row hour past present future');
-// timeBlockEl11.text('11am');
-
-// timeBlockEl12.addClass('time-block row hour past present future');
-// timeBlockEl12.text('12pm');
-
-// timeBlockEl13.addClass('time-block row hour past present future');
-// timeBlockEl13.text('1pm');
-
-// timeBlockEl14.addClass('time-block row hour past present future');
-// timeBlockEl14.text('2pm');
-
-// timeBlockEl15.addClass('time-block row hour past present future');
-// timeBlockEl15.text('3pm');
-
-// timeBlockEl16.addClass('time-block row hour past present future');
-// timeBlockEl16.text('4pm');
-
-// timeBlockEl17.addClass('time-block row hour past present future');
-// timeBlockEl17.text('5pm');
-
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var meridien = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var meridian = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 for (var i = 0; i < hours.length; i++) {
     var blockContainer = $(`
     <div id="hour-${hours[i]}" class="row time-block">
     <div class="col-md-1 hour">
-        ${meridien[i]}
+        ${meridian[i]}
     </div>
     <textarea class="col-md-10 description">
 </textarea>
@@ -76,7 +29,7 @@ function colorChange() {
     var hourBlock = parseInt($(this).attr("id").split("-")[1]);
     if (hourBlock < currentHour) {
         $(this).addClass('past')
-    } else if ( hourBlock === currentHour) {
+    } else if (hourBlock === currentHour) {
         $(this).addClass('present')
     } else {
         $(this).addClass('future')
